768424b197e5d2dedd2f9a064028bac09356f9ec
Thai Nguyen <thainhhe172393@fpt.edu.vn>
2024-06-19 11:36:50 +0700

    add ui for manage room

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/HomePage.js";
 import LoginForm from "./components/LoginForm.js";
 import UserProfile from "./components/UserProfile.js";
 import ListRoom from "./pages/ListRoom.js";
+import ManagerRoom from "./pages/ManagerRoom.js";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
           <Route path="/user/edit/:id" element={<Home />} />           {/*   edit profile student     */}
           <Route path="/manage/account" element={<Home />} />         {/*   admin manage account     */}
           <Route path="/user/:id" element={<Home />} />            {/*    view profile    */}
-          <Route path="/manage/room" element={<Home />} />              {/*     manage rooma dmin   */}
+          <Route path="/manage/room" element={<ManagerRoom />} />              {/*     manage rooma dmin   */}
           <Route path="/manage/room/edit/r:roomid" element={<Home />} />  {/* edit info room dorm*/}
           <Route path="/listroom" element={<ListRoom />} />                {/*   view list room in dom     */}
           <Route path="/manage/room/add" element={<Home />} />              {/*    add room admin    */}
---
+++ src/pages/ManagerRoom.js
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+
+import { Col, Row, Table,} from 'react-bootstrap';
+import { Pagination } from "antd"
+import { Link, useNavigate } from "react-router-dom";
+import LayoutAdmin from "../layout/LayoutAdmin";
+
+const ManagerRoom = () => {
+
+    const [bed, setBed] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage] = useState(5);
+
+
+    // Tính toán số trang
+    
+
+    // Lấy index bắt đầu và kết thúc của list user hiện tại
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentBed = bed.slice(indexOfFirstUser, indexOfLastUser);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    
+    const navigate = useNavigate()
+    useEffect(() => {
+      const role = sessionStorage.getItem('userrole');
+      const id = sessionStorage.getItem('id');
+      if (role !== "admin" || id === null) {
+        navigate("/manage/room");
+      }
+    }, []);
+    useEffect(() => {
+        fetch('http://localhost:9999/dormitories')
+            .then(resp => resp.json())
+            .then(data => {
+                setBed(data);
+               
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    }, []);
+
+    return (
+        <LayoutAdmin>
+            <Row >
+                <Col xs={12}>
+                    <Row>
+                        <Col style={{ textAlign: 'center' }}>
+                            <h2>Manager Room</h2>
+                        </Col>
+                    </Row>
+
+
+                    <Row>
+                        <Col>
+                            <Table className="box-shadow 2px ">
+                                <thead>
+                                    <tr>
+
+                                        <th>Dom Name</th>
+                                        <th>Dom ID</th>
+                                        <th>Total Bed</th>
+                                        <th>Used Bed</th>
+                                        <th>Free Bed</th>
+                                        <th>Action</th>
+
+
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        currentBed.map(b => (
+                                            <tr key={b.id}>
+
+                                                <td>{b.domName}</td>
+                                                <td>{b.domID}&ensp;-&ensp;{b.slot}</td>
+                                                <td>{b.totalBed}</td>
+                                                <td>{b.usedBed}</td>
+                                                <td>{b.freeBed}</td>
+                                                <td><Link to={'/edit/room/'+b.id}>Edit</Link></td>
+                                            </tr>
+                                        ))
+                                    }
+                                    <tr>
+                                        {/* <td colSpan="2">Total</td>
+                                        <td>
+                                            {
+                                                currentBed.reduce((total, b) => total + b.totalBed, 0)
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                currentBed.reduce((total, b) => total + b.usedBed, 0)
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                currentBed.reduce((total, b) => total + b.freeBed, 0)
+                                            }
+                                        </td> */}
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            <Pagination
+                                current={currentPage}
+                                total={bed.length}
+                                pageSize={usersPerPage}
+                                onChange={paginate}
+
+                                style={{ marginTop: "16px", textAlign: "center" }}
+                            />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
+        </LayoutAdmin>
+    );
+}
+
+export default ManagerRoom;
