1257766abfc3a751e9d33e47acb24b2c39129c8e
Hminh11 <minhvhhe170320@fpt.edu.vn>
2024-07-06 22:34:57 +0700

    fixing

--- src/pages/ManagePendingBooking.js
+++ src/pages/ManagePendingBooking.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import TemplateAdmin from "../layout/LayoutAdmin";
 import axios from 'axios';
 export default function ManagePendingBooking() {
     const [bookingRequests, setBookingRequests] = useState([]);
@@ -106,68 +106,70 @@ export default function ManagePendingBooking() {
     };
 
     return (
-        <div className="container mt-5">
-            <div className="d-flex justify-content-center row">
-                <div className="col-md-10">
-                    <div className="rounded">
-                        <div className="table-responsive table-borderless table-striped">
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th>BookingID</th>
-                                        <th>Name Student</th>
-                                        <th>Dom</th>
-                                        <th>Floor</th>
-                                        <th>Room</th>
-                                        <th>Bed</th>
-                                        <th>Semester</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="table-body">
-                                    {bookingRequests?.map((request) => {
+        <TemplateAdmin>
+            <div className="container mt-5">
+                <div className="d-flex justify-content-center row">
+                    <div className="col-md-10">
+                        <div className="rounded">
+                            <div className="table-responsive table-borderless table-striped">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>BookingID</th>
+                                            <th>Name Student</th>
+                                            <th>Dom</th>
+                                            <th>Floor</th>
+                                            <th>Room</th>
+                                            <th>Bed</th>
+                                            <th>Semester</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="table-body">
+                                        {bookingRequests?.map((request) => {
 
-                                        const student = users?.find(user => user.studentID == request.studentid);
-                                        const dorm = dormitories?.find(dorm => dorm.id == request.dormitory)
-                                        const floor = dorm.floors.find(fl => fl.id.toString() === request.floor.toString());
-                                        const room = floor.rooms.find(rm => rm.id.toString() === request.room.toString());
-                                        const bed = room.beds.find(bd => bd.id.toString() === request.bed.toString());
-                                        const statusClass = request.status === 'approved' ? 'success' : request.status === 'pending' ? 'info' : 'danger';
-                                        return (
-                                            <tr key={request.id} className="cell-1">
-                                                <td>{request.id}</td>
-                                                <td>{student?.fullName}</td>
-                                                <td>{dormitories?.find(dorm => dorm.id == request.dormitory).name}</td>
-                                                <td>{floor.floorNumber}</td>
-                                                <td>{room.roomNumber}</td>
-                                                <td>{bed.name}</td>
-                                                <td>{request.semester}</td>
-                                                <td><span className={`badge badge-${statusClass}`}>{request.status}</span></td>
-                                                <td>
-                                                    {request.status === 'pending' ? (
-                                                        <>
-                                                            <Link onClick={() => handleOnAprove(request.id, request.studentid, request.dormitory, request.floor, request.room, request.bed)}>
-                                                                <i className="confirmed">&#10004;</i>
-                                                            </Link>
-                                                            <Link onClick={() => handleOnReject(request.id)}>
-                                                                <i className="cancelled">&#10008;</i>
-                                                            </Link>
-                                                        </>
-                                                    ) : (
-                                                        <i className="block">&#128683;</i>
-                                                    )}
-                                                </td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
+                                            const student = users?.find(user => user.studentID == request.studentid);
+                                            const dorm = dormitories?.find(dorm => dorm.id == request.dormitory)
+                                            const floor = dorm.floors.find(fl => fl.id.toString() === request.floor.toString());
+                                            const room = floor.rooms.find(rm => rm.id.toString() === request.room.toString());
+                                            const bed = room.beds.find(bd => bd.id.toString() === request.bed.toString());
+                                            const statusClass = request.status === 'approved' ? 'success' : request.status === 'pending' ? 'info' : 'danger';
+                                            return (
+                                                <tr key={request.id} className="cell-1">
+                                                    <td>{request.id}</td>
+                                                    <td>{student?.fullName}</td>
+                                                    <td>{dormitories?.find(dorm => dorm.id == request.dormitory).name}</td>
+                                                    <td>{floor.floorNumber}</td>
+                                                    <td>{room.roomNumber}</td>
+                                                    <td>{bed.name}</td>
+                                                    <td>{request.semester}</td>
+                                                    <td><span className={`badge badge-${statusClass}`}>{request.status}</span></td>
+                                                    <td>
+                                                        {request.status === 'pending' ? (
+                                                            <>
+                                                                <Link onClick={() => handleOnAprove(request.id, request.studentid, request.dormitory, request.floor, request.room, request.bed)}>
+                                                                    <i className="confirmed">&#10004;</i>
+                                                                </Link>
+                                                                <Link onClick={() => handleOnReject(request.id)}>
+                                                                    <i className="cancelled">&#10008;</i>
+                                                                </Link>
+                                                            </>
+                                                        ) : (
+                                                            <i className="block">&#128683;</i>
+                                                        )}
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </table>
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </TemplateAdmin>
     );
 }
