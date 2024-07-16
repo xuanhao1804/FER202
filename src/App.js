ae9aa38fcc9b2fb3395852c6047ef987c9896d33
xuanhao1804 <xuanhao1804@gmail.com>
2024-07-16 23:17:34 +0700

    .

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ import BookingRequests from "./pages/user/BookingRequests.js";
 import ChangePassword from "./pages/user/ChangePassword.js";
 import AboutUs from "./pages/AboutUs.js";
 import AdminRegulations from "./pages/AdminRegulations.js";
+import News from "./components/News.js";
+import ManageNewsAdmin from "./components/ManageNewsAdmin.js";
 
 
 function App() {
@@ -47,6 +49,10 @@ function App() {
           <Route path="/booking2" element={<DormitorySelector />} />  
           <Route path="/booking-requests" element={<BookingRequests />} />    
           <Route path="/changepass" element={<ChangePassword />} />    
+          <Route path="/viewnews" element={<News/>      } />      
+          <Route path="/manage/news" element={<ManageNewsAdmin/>    } />
+
+
 
         
           <Route path="/manage/regulation" element={<AdminRegulations />} />       
